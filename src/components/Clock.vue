12444cb94fbf85d9ce946c75a35e4137d17353ef
Noman Hasan <nomanbinhussein@gmail.com>
2018-06-10 23:03:08 +0600

    interval added

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,26 +1,59 @@
 <template>
-  <div class="hello">
+  <div class="clock">
     <h1> {{(format(time, 'hh:MM:ss A'))}} </h1>
     
     <button class="button-primary">Button element</button>
+
+    <div class="main">
+      <div class="logs">
+        <div class="time-log" v-for="t of intervalTimeLogs" :key="t.getTime()" >
+          <div class="time-span">
+            {{(format(t, 'hh:MM:ss A'))}}
+          </div>
+        </div>
+      </div>
+      <div class="messages">
+        {{ stopwatch }}
+      </div>
+    </div>
     
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import { format } from 'date-fns';
+import { format, differenceInSeconds, differenceInMinutes, differenceInHours, differenceInMilliseconds } from 'date-fns';
+import * as R from 'ramda';
 
 @Component
 export default class HelloWorld extends Vue {
   
   time = new Date();
+
+  intervalSeconds = 105;
+  stopwatch = '';
+
+  intervalTimeLogs: Array<Date> = [];
   
   @Prop() private msg!: string;
 
   mounted() {
+
+    this.time = new Date();
+    this.intervalTimeLogs.push(new Date(this.time));
+
     setInterval(() => {
-      this.time = new Date()
+      this.time = new Date();
+      const last: Date = this.intervalTimeLogs[this.intervalTimeLogs.length - 1];
+
+      this.stopwatch = `${differenceInHours(this.time, last)}:${differenceInMinutes(this.time, last)}:${differenceInSeconds(this.time, last)}: ${differenceInMilliseconds(this.time, last)}`
+
+      // this.stopwatch = format(Date.UTC(this.time - last), 'hh:MM:ss A');
+
+      if (differenceInSeconds(this.time, last) >= this.intervalSeconds) {
+        this.intervalTimeLogs.push(new Date(this.time));
+      }
+
     }, 1000);
   }
   format = format;
@@ -43,4 +76,18 @@ li {
 a {
   color: #42b983;
 }
+
+.main {
+  height: calc(100vh - 210px);
+
+  display: flex;
+  flex-direction: row;
+
+  .logs, .messages {
+    flex: 1;
+    border: 1px solid grey;
+    height: 100%;
+  }
+}
+
 </style>
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+export interface VuexStore {
+  name: string;
+}
 
 export default new Vuex.Store({
   state: {
